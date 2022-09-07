b5c960c7a274585a0e66c708b948981fb9d1e371
liutao <africa19891207@gmail.com>
2022-09-07 09:47:35 +0800

    '初始化formily'

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
-<JsonSchemaForm />
+
 </template>
 
 <script lang="ts" setup>
-// import JsonSchemaForm from '@/components/JsonSchemaForm'
-import JsonSchemaForm from '@/components/Upload/UploadDemo.vue'
+
 </script>
