03076215dbe751d8b90fa8ea263cdc5c65701b26
liutao <africa19891207@gmail.com>
2022-09-08 21:13:16 +0800

    demo

--- src/api/test.ts
+++
@@ -1,9 +0,0 @@
-import request from "@/utils/http";
-
-import { DemoResponse } from "@/@types";
-
-
-export const getList = (data: any) => {
-  return request.get<DemoResponse>("/author/recommend", data);
-};
-
