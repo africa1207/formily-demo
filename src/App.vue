0eb71522d70d1f5acd765e74c75601d3b0b29100
liutao <africa19891207@gmail.com>
2022-09-08 21:13:16 +0800

    demo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-
+  <demo></demo>
 </template>
 
 <script lang="ts" setup>
-
+import Demo from '@/components/demo.vue'
 </script>
---
+++ src/components/demo.vue
@@ -0,0 +1,36 @@
+<template>
+  <FormProvider :form="form">
+    <ArrayField
+        name="upload"
+        title="卡片上传"
+        :decorator="[FormItem]"
+        :component="[
+        Upload,
+        {
+          listType: 'picture-card',
+          action:'//localhost:8008/upload',
+          name: 'file'
+        },
+      ]"
+        required
+    />
+    <FormButtonGroup>
+      <Submit @submit="handleSubmit">提交</Submit>
+      <Reset forceClear>重置</Reset>
+    </FormButtonGroup>
+  </FormProvider>
+</template>
+
+<script lang="ts" setup>
+import {createForm} from '@formily/core'
+import {FormItem, FormButtonGroup, Submit, Reset, Upload} from '@formily/element-plus'
+import {FormProvider, ArrayField, useField} from '@formily/vue'
+
+
+const form = createForm()
+const field = useField()
+const handleSubmit = (data) => {
+  console.log(data)
+}
+
+</script>
